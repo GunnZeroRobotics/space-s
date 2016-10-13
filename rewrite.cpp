1d1efff2e44fc4330046c1b485751dc57bea9fae
Kevin Li <kevinli120@yahoo.com>
2016-10-12 20:03:45 -0700

    quick fix

--- rewrite.cpp
+++ rewrite.cpp
@@ -61,8 +61,9 @@ void loop()
     else
     {
         int IDcount = 1;
-        while (game.hasItemBeenPickedUp(IDcount) && game.hasItem(IDcount) != 1)
+        while (game.itemInZone(IDcount))
         {
+            if (game.hasItem(IDcount) == 1) {break;}
             IDcount--;
             if (IDcount < 0)
             {
